244bc26802053c817d3c90bdca90904e5834a9d4
Matej Zidek <2239018z@student.gla.ac.uk>
2018-02-09 02:52:29 +0000

    Chapter 8v0

--- rango/views.py
+++ rango/views.py
@@ -54,14 +54,14 @@ def add_page(request, category_name_slug):
    form = PageForm()
    if request.method == 'POST':
       form = PageForm(request.POST)
-   if form.is_valid():
-      if category:
-         page = form.save(commit=False)
-         page.category = category
-         page.views = 0
-         page.save()
-         return show_category(request, category_name_slug)
-   else:
-      print(form.errors)
+      if form.is_valid():
+         if category:
+            page = form.save(commit=False)
+            page.category = category
+            page.views = 0
+            page.save()
+            return show_category(request, category_name_slug)
+      else:
+         print(form.errors)
    context_dict = {'form':form, 'category': category}
    return render(request, 'rango/add_page.html', context_dict)
